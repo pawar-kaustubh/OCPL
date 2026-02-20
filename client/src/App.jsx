852af5a4c6995954542b4cf7fe61be444254dc91
Kaustubh Pawar <167971412+pawar-kaustubh@users.noreply.github.com>
2026-02-20 10:10:37 +0530

    Add pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,13 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
